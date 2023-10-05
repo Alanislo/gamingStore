3ad723ea2bcc070b011f772fd8fe3e403ed35323
GabiBarbera <gabriel.barberaa@gmail.com>
2023-10-04 21:32:30 -0300

    a la perca

--- src/main/resources/static/web/scripts/cart.js
+++ src/main/resources/static/web/scripts/cart.js
@@ -9,10 +9,23 @@ const options = {
             productosMouse: undefined,
             productosKeyboard: undefined,
             productosMicrophone: undefined,
-            productosHeadphone: undefined
+            productosHeadphone: undefined,
+            isOverlayVisible: false,
+            buttonTexts: ['Log In', 'Register'],
+            currentIndex: 0,
+            isLoggedIn: false,
+            client: [],
         }
     },
     created() {
+        this.loadData()
+        setInterval(this.changeButtonText, 2000);
+        const isLoggedIn = localStorage.getItem('isLoggedIn');
+        if (isLoggedIn === "true") {
+            this.isLoggedIn = true;
+        } else {
+            this.isLoggedIn = false;
+        }
         if (localStorage.getItem('carrito') != null) {
             this.cantidadTotalProductos = JSON.parse(localStorage.getItem('cantidadTotalProductos'))
             this.productosMouse = JSON.parse(localStorage.getItem('productosMouse'))
@@ -31,6 +44,14 @@ const options = {
                     this.productosHeadphone = data.filter(producto => producto.category == 'headphone')
                 })
                 .catch(error => console.log(error))
+        }
+    },
+    computed: {
+        contadorCarrito() {
+            this.cantidadTotalProductos = this.carrito.reduce((acc, producto) => acc + producto.cantidadEnCarrito, 0);
+        },
+        buttonText() {
+            return this.buttonTexts[this.currentIndex];
         }
     },
     methods: {
@@ -208,13 +229,66 @@ const options = {
         },
         actualizarTotal() {
             this.totalPrecio = this.carrito.reduce((acc, producto) => acc + Number(producto.precio * producto.cantidadEnCarrito), 0)
+        },
+        loadData() {
+            axios.get('/api/clients/current')
+                .then(response => {
+                    this.client = response.data
+                    console.log(this.client);
+                })
+                .catch(error => console.error('Error:', error));
+        },
+        showOverlay() {
+            this.isOverlayVisible = true;
+        },
+        hideOverlay() {
+            this.isOverlayVisible = false;
+        },
+        changeButtonText() {
+            this.currentIndex = (this.currentIndex + 1) % this.buttonTexts.length;
+        },
+        logout() {
+            Swal.fire({
+                title: 'Do you want to log out of your account?',
+                inputAttributes: {
+                    autocapitalize: 'off',
+                },
+                showCancelButton: true,
+                confirmButtonText: 'Sure',
+                showLoaderOnConfirm: true,
+                preConfirm: (login) => {
+                    axios.post(`/api/logout`)
+                        .then(response => {
+                            this.isLoggedIn = false;
+                            localStorage.removeItem('isLoggedIn');
+                            Swal.fire({
+                                icon: 'succes',
+                                title: response.data,
+                                text: `You have successfully logged out.`,
+                                customClass: {
+                                    popup: 'custom-alert',
+                                }
+                            });
+                            setTimeout(() => {
+                                window.location.href = "../../index.html"
+                            }, 2000);
+                        })
+                        .catch(error => {
+                            console.error('Error:', error);
+                            Swal.fire({
+                                icon: 'error',
+                                title: error.response.data,
+                                confirmButtonText: 'OK',
+                                customClass: {
+                                    popup: 'custom-alert',
+                                }
+                            });
+                        })
+                },
+                allowOutsideClick: () => !Swal.isLoading(),
+            })
         }
 
-    },
-    computed: {
-        contadorCarrito() {
-            this.cantidadTotalProductos = this.carrito.reduce((acc, producto) => acc + producto.cantidadEnCarrito, 0);
-        }
     }
 }
 
