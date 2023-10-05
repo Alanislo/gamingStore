551f8ee7ea4c0d59cdeecc188ca40b401625d87c
Daryl Baptie <50216607+DarylBaptie@users.noreply.github.com>
2023-10-04 21:31:36 -0300

    update for merge

--- src/main/resources/static/web/scripts/products.js
+++ src/main/resources/static/web/scripts/products.js
@@ -21,9 +21,12 @@ createApp({
     productColor: [],
     productColorReduced: [],
     productColorFinal: [],
+    productsFilteredByNav: [],
     ranges: [{label: 'Under $100', min: 0, max: 99.99}, {label: '$100 - $200', min: 100, max: 199.99}, {label: '$200 - $300', min: 200, max: 299.99}, {label: 'Over $300', min: 300, max: 1000000}],
     priceMin : null,
     priceMax: null,
+    parameter: null,
+    params: null,
     }
   },
   created() {
@@ -38,7 +41,11 @@ createApp({
               .then((response) => {
                console.log(response);
                this.products = response.data;
+               this.parameter = location.search;
+               this.params = new URLSearchParams(this.parameter);
+               this.categoryChosen = this.params.get("parameter");
                this.productsFiltered = response.data;
+               this.SelectedProductsByNav = this.products.filter((product) => product.category == this.categoryChosen)
                this.productCategory = [...new Set(this.products.map((product) => product.category))];
                this.productBrand = [...new Set(this.products.map((product) => product.brand))];
                this.productColor = [...new Set(this.products.map((product) => product.colors))];
@@ -81,30 +88,31 @@ createApp({
   } else if (product.price >= 300) {
   product.priceRange = "Over $300"
   }
-
   }
-  }
-
-
+  },
   },
   computed: {
   priceRange() {
-
         this.priceRange = this.prices.map(range => range.min)
   },
-
-
-
     filter() {
         this.priceMin = Math.min(...this.prices.map(range => range.min)) ;
         this.priceMax = Math.max(...this.prices.map(range => range.max)) ;
-        this.productsFiltered = this.products.filter(
-        (product) =>
-         (this.brands.includes(product.brand) || this.brands.length == 0) &&
-         (this.categories.includes(product.category) || this.categories.length == 0) &&
-         (product.colors.some(color => this.colors.includes(color)) || this.colors.length == 0) &&
-         (this.prices.includes(product.priceRange) || this.prices.length == 0)
-      );
+        if(!this.categoryChosen) {
+                this.productsFiltered = this.products.filter(
+                (product) =>
+                 (this.brands.includes(product.brand) || this.brands.length == 0) &&
+                 (this.categories.includes(product.category) || this.categories.length == 0) &&
+                 (product.colors.some(color => this.colors.includes(color)) || this.colors.length == 0) &&
+                 (this.prices.includes(product.priceRange) || this.prices.length == 0)
+        )} else if (this.categoryChosen) {
+                        this.productsFiltered = this.SelectedProductsByNav.filter(
+                        (product) =>
+                         (this.brands.includes(product.brand) || this.brands.length == 0) &&
+                         (this.categories.includes(product.category) || this.categories.length == 0) &&
+                         (product.colors.some(color => this.colors.includes(color)) || this.colors.length == 0) &&
+                         (this.prices.includes(product.priceRange) || this.prices.length == 0)
+              )}
     },
   },
 }).mount('#app')
